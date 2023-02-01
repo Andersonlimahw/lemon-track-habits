68eb7278251dca025389dea63364cbbec0e8806b
Anderson Lima <andersonlimahw@gmail.com>
2023-02-01 10:57:05 -0300

    🆕 [Web] - setup

---
+++ web-hack-habits/tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/*.tsx',
+    './src/**/*.tsx',
+    './src/**/**/*.tsx',
+    './src/**/**/**/*.tsx',
+    './index.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
