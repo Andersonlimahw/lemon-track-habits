65b7b9136b9f52ab729d2002ce07b48e068fa058
Anderson Lima <andersonlimahw@gmail.com>
2023-02-01 19:45:21 -0300

    [API] - Adding day route

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,10 +1,64 @@
 import { prisma } from './lib/prisma';
 import { FastifyInstance } from 'fastify';
-
+import { z } from 'zod';
+import dayjs from '../node_modules/dayjs/esm';
 
 export async function appRoutes(app : FastifyInstance) {
-    app.get('/',  async () => {
+    app.get('/habits',  async () => {
         const habits = await prisma.habit.findMany();
         return habits;
     });
+
+    app.post('/habits', async (request) => {
+        const habbitInput = z.object({
+            title: z.string(),
+            weekDays: z.array(
+                z.number()
+                .min(0)
+                .max(6)
+            )
+        })
+        const { title, weekDays }  = habbitInput.parse(request.body);
+
+        const today = dayjs().startOf('day').toDate();
+        await prisma.habit.create({
+            data: {
+                title, 
+                created_at: today, 
+                weekDays: {
+                    create: weekDays.map(weekDay => {
+                        return {
+                            week_day: weekDay
+                        }
+                    })
+                }
+            }
+        })
+    })
+
+    app.get('/day', async (request) => {
+        const dayParamsInput = z.object({
+            date: z.coerce.date()
+        });
+
+        const { date } = dayParamsInput.parse(request.query);
+        const weekDay = dayjs(date).get('day');
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date,
+                }, 
+                weekDays: {
+                   some: {
+                    week_day: weekDay
+                   }
+                }
+            }
+        });
+
+        return {
+            possibleHabits
+        };
+    })
 }
