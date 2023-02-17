8c80f9d0665141d07050291f0148470829d6bd99
Anderson Lima <andersonlimahw@gmail.com>
2023-02-17 20:30:27 -0300

    [Api] - Adicionando patch para toggle de hábitos

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,104 +1,146 @@
-import dayjs from 'dayjs';
-import { prisma } from './lib/prisma';
-import { FastifyInstance } from 'fastify';
-import { z } from 'zod';
+import dayjs from "dayjs";
+import { prisma } from "./lib/prisma";
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
 
+export async function appRoutes(app: FastifyInstance) {
+  app.post("/habits", async (request) => {
+    const habbitInput = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+    const { title, weekDays } = habbitInput.parse(request.body);
 
-export async function appRoutes(app : FastifyInstance) {
-    app.get('/habits',  async () => {
-        const habits = await prisma.habit.findMany({
-            include: {
-                dayHabits: true, 
-                weekDays: true
-            }
-        });
-        return habits;
+    const today = dayjs().startOf("day").toDate();
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map((weekDay) => {
+            return {
+              week_day: weekDay,
+            };
+          }),
+        },
+      },
+    });
+  });
+
+  app.get("/habits", async () => {
+    const habits = await prisma.habit.findMany({
+      include: {
+        dayHabits: true,
+        weekDays: true,
+      },
+    });
+    return habits;
+  });
+
+  app.patch("/habits/:id/toggle", async (request) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
     });
 
-    app.post('/habits', async (request) => {
-        const habbitInput = z.object({
-            title: z.string(),
-            weekDays: z.array(
-                z.number()
-                .min(0)
-                .max(6)
-            )
-        })
-        const { title, weekDays }  = habbitInput.parse(request.body);
+    const { id } = toggleHabitParams.parse(request.params);
+    const today = dayjs().startOf("day").toDate();
 
-        const today = dayjs().startOf('day').toDate();
-        await prisma.habit.create({
-            data: {
-                title, 
-                created_at: today, 
-                weekDays: {
-                    create: weekDays.map(weekDay => {
-                        return {
-                            week_day: weekDay
-                        }
-                    })
-                }, 
-            }
-        })
-    })
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
 
-    app.get('/days', async (request) => {
-        const days = await prisma.day.findMany({
-            include: {
-                dayHabits: true,
-            }
-        });
-        return days;
-    })
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
 
-    app.get('/day', async (request) => {
-        const dayParamsInput = z.object({
-            date: z.coerce.date()
-        });
-
-        const { date } = dayParamsInput.parse(request.query);
-        const weekDay = dayjs(date).get('day');
-        const parsedDate = dayjs(date).startOf('day');
-
-        const possibleHabits = await prisma.habit.findMany({
+    if (dayHabit) {
+        // Deleta hábito caso exista.
+        await prisma.dayHabit.delete({
             where: {
-                created_at: {
-                    lte: date,
-                }, 
-                weekDays: {
-                   some: {
-                    week_day: weekDay
-                   }
-                }
-            }, 
-            include: {
-                dayHabits: true,
-                weekDays: true,
-            }
-        });
-        
-        const day = await prisma.day.findUnique({
-            where: {
-                date: parsedDate.toDate(),
-            }, 
-            include: {
-                dayHabits: true,
+                id: dayHabit.id
             }
         })
+    } else {
+      // Completa o habito:
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
+  });
 
+  app.get("/days", async (request) => {
+    const days = await prisma.day.findMany({
+      include: {
+        dayHabits: true,
+      },
+    });
+    return days;
+  });
 
-        const completedHabits = day?.dayHabits.map(dayHabit => {
-            return dayHabit.habit_id
-        }) ?? [];
+  app.get("/day", async (request) => {
+    const dayParamsInput = z.object({
+      date: z.coerce.date(),
+    });
 
-        return {
-            data: {
-                possibleHabits, 
-                completedHabits
-            }, 
-            message: "Success", 
-            code: "200"
-           
-        };
-    })
+    const { date } = dayParamsInput.parse(request.query);
+    const weekDay = dayjs(date).get("day");
+    const parsedDate = dayjs(date).startOf("day");
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          },
+        },
+      },
+      include: {
+        dayHabits: true,
+        weekDays: true,
+      },
+    });
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+
+    const completedHabits =
+      day?.dayHabits.map((dayHabit) => {
+        return dayHabit.habit_id;
+      }) ?? [];
+
+    return {
+      data: {
+        possibleHabits,
+        completedHabits,
+      },
+      message: "Success",
+      code: "200",
+    };
+  });
 }
