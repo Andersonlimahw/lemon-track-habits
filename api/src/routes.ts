1346a54cd6a198aa6ab81245585ea90cc57f3888
Anderson Lima <andersonlimahw@gmail.com>
2023-02-19 15:40:16 -0300

    [API] - adicionando nova rota /summary

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -70,13 +70,13 @@ export async function appRoutes(app: FastifyInstance) {
 
     // Delete or insert:
     if (dayHabit) {
-        // Deleta hábito caso exista.
-        await prisma.dayHabit.delete({
-            where: {
-                id: dayHabit.id
-            }
-        })
-        message = "removed";
+      // Deleta hábito caso exista.
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+      message = "removed";
     } else {
       // Completa o habito:
       await prisma.dayHabit.create({
@@ -90,14 +90,14 @@ export async function appRoutes(app: FastifyInstance) {
     return {
       data: {
         habitId: id,
-        daiId: day.id
+        daiId: day.id,
       },
       message: `Success, Habit ${message} with success`,
       code: "200",
     };
   });
 
-  app.get("/days", async (request) => {
+  app.get("/days", async () => {
     const days = await prisma.day.findMany({
       include: {
         dayHabits: true,
@@ -153,6 +153,42 @@ export async function appRoutes(app: FastifyInstance) {
       },
       message: "Success",
       code: "200",
+    };
+  });
+
+  // Retorna hábitos possíveis.
+  // Retorna quantos hábitos foram completados
+  // Query customizada para reduzir chamadas para o banco de dados que o ORM faz.
+  // Prisma ORM:
+  // RAW : query customizada especifica com o banco selecionado no caso desta aplicação o sqllite.
+  app.get("/summary", async () => {
+    const summary = await prisma.$queryRaw`
+      SELECT 
+        day.id,
+        day.date,
+        (
+          SELECT 
+            CAST(COUNT(*) AS FLOAT)
+          FROM days_habits dayHabit
+          WHERE dayHabit.day_id = day.id
+        ) as completed,
+        (
+          SELECT 
+            CAST(COUNT(*) AS FLOAT)
+          FROM habit_week_days weekDays
+          JOIN habits habit
+            ON habit.id = weekDays.habit_id
+          WHERE
+            weekDays.week_day = CAST(strftime('%w',day.date/1000.0,'unixepoch') AS INT)
+            AND habit.created_at <= day.date
+        ) as amount
+      FROM days day
+    `;
+
+    return {
+      data: summary,
+      code: "200",
+      message: "Success",
     };
   });
 }
